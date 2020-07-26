0d3e6a3891ab4354a990e71de79b30e0a80a9964
mayank8aug <mayankkumar@lenskart.in>
2020-07-26 20:03:46 +0530

    Added event handlers

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 class App extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            data: null,
+            serverError: false
+        };
         this.fileUploadHandler = this.fileUploadHandler.bind(this);
+        this.successHandler = this.successHandler.bind(this);
+        this.errorHandler = this.errorHandler.bind(this);
+        this.cleanUpStates = this.cleanUpStates.bind(this);
     }
 
     fileUploadHandler() {
@@ -12,14 +19,38 @@ class App extends React.Component {
         formData.append('file', file);
         xhr.open('post', 'abc.com', true);
         xhr.setRequestHeader("Content-Type", "multipart/form-data");
+        xhr.onload = function() {
+            if (xhr.status != 200) {
+                this.errorHandler()
+            } else {
+                this.successHandler(xhr.response);
+            }
+        };
+        xhr.onerror = function() {
+            this.errorHandler();
+        }
         xhr.send(formData);
     }
 
+    successHandler(response) {
+        this.setState({ data: response });
+    }
+
+    errorHandler() {
+        this.setState({ serverError: true });
+    }
+
+    cleanUpStates() {
+        this.setState({ serverError: false });
+    }
+
     render() {
+        const { serverError } = this.state;
         return (
             <div>
-                <input type="file" id="videoId" />
-                <button onClick={this.fileUploadHandler}>Analyse Data</button>
+                <input className="block" onChange={this.cleanUpStates} type="file" id="videoId" />
+                <button className="block" onClick={this.fileUploadHandler}>Analyse Data</button>
+                {serverError && <span className="block error">System Error Occuured...</span>}
             </div>
         );
     }
